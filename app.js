d1b2872d12661071dc534234386c277cc79337a2
Abhinandan Kumar <abhicpr05@gmail.com>
2025-08-31 18:27:35 +0530

    add data

---
+++ app.js
@@ -0,0 +1,28 @@
+import Store from "./services/Store.js";
+import API from "./services/API.js";
+import { loadData } from "./services/Menu.js";
+
+// ? Alias
+/*
+const $ = () => document.querySelector.call(this, arguments);
+
+const $$ = () => document.querySelectorAll.call(this, arguments);
+
+HTMLElement.prototype.on = (a, b, c) => this.addEventListener(a, b, c);
+
+HTMLElement.prototype.off = (a, b) => this.removeEventListener(a, b);
+
+HTMLElement.prototype.$ = (s) => this.querySelector(s);
+
+HTMLElement.prototype.$ = (s) => this.querySelectorAll(s);
+
+*/
+
+window.app = {};
+app.store = Store;
+
+// ! It's better to wait for the event for manipulation
+
+window.addEventListener("DOMContentLoaded", async () => {
+  loadData();
+});
